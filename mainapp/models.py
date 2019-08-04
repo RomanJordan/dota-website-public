94cc470219247c3c9739030a9e45d876f4ac7685
RomanJordan <32529400+RomanJordan@users.noreply.github.com>
2019-08-03 22:12:28 -0400

    Expanded models

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from PIL import Image
 from django.utils.text import slugify
 import itertools
+from multiselectfield import MultiSelectField
+
 
 class Hero(models.Model):
     STRENGTH = 'STR'
@@ -16,11 +18,24 @@ class Hero(models.Model):
         ('INTELLIGENCE', 'Intelligence'),
     ]
 
+    HERO_ROLES = (('RANGED', 'Ranged'),
+              ('MELEE', 'Melee'),
+              ('CARRY', 'Carry'),
+              ('SUPPORT', 'Support'),
+              ('NUKER', 'Nuker'),
+              ('DISABLER', 'Disabler'),
+              ('JUNGLER', 'Jungler'),
+              ('DURABLE', 'Durable'),
+              ('ESCAPE', 'Escape'),
+              ('PUSHER', 'Pusher'),
+              ('INITIATOR', 'Initiator'),
+              ('ROAMER', 'Roamer'),)
 
     hero_name = models.CharField(max_length=50, default='Hero')
     hero_thumbnail = models.ImageField(default='Juggernaut_icon.png', upload_to='hero_thumbnails')
     hero_lore = models.TextField(default='Default hero lore')
     hero_main_attribute = models.CharField(choices=ATTRIBUTES, default=STRENGTH, max_length=12)
+    hero_attribute_image = models.ImageField(default='Strength_attribute_symbol.png', upload_to='hero_thumbnails')
     hero_base_health = models.IntegerField(default=1000)
     hero_base_mana = models.IntegerField(default=25)
     hero_base_magic_res = models.IntegerField(default=25)
@@ -30,9 +45,23 @@ class Hero(models.Model):
     hero_base_att_persec = models.IntegerField(default=25)
     hero_base_damage = models.IntegerField(default=25)
     hero_base_movement_speed = models.IntegerField(default=25)
-    hero_attribute_image = models.ImageField(default='Strength_attribute_symbol.png', upload_to='hero_thumbnails')
+    hero_roles = MultiSelectField(max_choices=6, choices=HERO_ROLES, blank=True)
     hero_counters = models.ManyToManyField("self", blank=True, symmetrical=False)
     hero_advantages = models.ManyToManyField("self", blank=True, symmetrical=False, related_name='advantages')
+    
+
+    hero_talent_left_lvl10 = models.CharField(default='default talent description', max_length=100)
+    hero_talent_right_lvl10 = models.CharField(default='default talent description', max_length=100)
+    
+    hero_talent_left_lvl15 = models.CharField(default='default talent description', max_length=100)
+    hero_talent_right_lvl15 = models.CharField(default='default talent description', max_length=100)
+    
+    hero_talent_left_lvl20 = models.CharField(default='default talent description', max_length=100)
+    hero_talent_right_lvl20 = models.CharField(default='default talent description', max_length=100)
+    
+    hero_talent_left_lvl25 = models.CharField(default='default talent description', max_length=100)
+    hero_talent_right_lvl25 = models.CharField(default='default talent description', max_length=100)
+    
     
     slug = models.SlugField(max_length=30, null=True, blank=True)
     
