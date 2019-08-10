61af6ea6198c3ba22f78b3e314198c349e7f6ab7
RomanJordan <32529400+RomanJordan@users.noreply.github.com>
2019-08-09 22:07:06 -0400

    Settings

--- dota_website/settings.py
+++ dota_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%an_&%c_stu(8)#4mz=!kaahlb-s-4an8%k#j1=-m=v8*o$w5!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dota2-counters.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
