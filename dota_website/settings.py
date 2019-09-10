227e961045d645e1eeba0c834b893459793cc8ff
RomanJordan <32529400+RomanJordan@users.noreply.github.com>
2019-09-10 00:11:05 -0400

    first commit

--- dota_website/settings.py
+++ dota_website/settings.py
@@ -141,11 +141,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'dota2bucket'
+AWS_STORAGE_BUCKET_NAME = ''
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % 'dota2bucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % ''
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
